d43eb93ed5d597ac61ddbea16803d3cdd9faba50
Brad Pizzimenti <brad@gennakersystems.com>
2016-09-17 12:03:59 -0700

    beginning of android lifecycle

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -8,16 +8,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private static final String KEY_FACT = "KEY_FACT";
+    private static final String KEY_COLOR = "KEY_COLOR";
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColorWheel = new ColorWheel();
     // Declare our view variables
     private TextView mFactTextView;
     private Button mShowFactButton;
     private RelativeLayout mRelativeLayout;
+    private String mFact;
+    private int mColor;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(KEY_FACT, mFact);
+        outState.putInt(KEY_COLOR, mColor);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +46,19 @@ public class FunFactsActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String fact = mFactBook.getFact();
+                mFact = mFactBook.getFact();
                 int color = mColorWheel.getColor();
 
 
                 //Update the screen with the new fact
-                mFactTextView.setText(fact);
-                mRelativeLayout.setBackgroundColor(color);
+                mFactTextView.setText(mFact);
+                mRelativeLayout.setBackgroundColor(mColor);
+                mShowFactButton.setTextColor(mColor);
             }
         };
         mShowFactButton.setOnClickListener(listener);
+
+//        Toast.makeText(FunFactsActivity.this, "Yay! Activity created!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We are logging from onCreate() method");
     }
 }
